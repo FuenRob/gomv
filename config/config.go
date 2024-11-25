3a8472638e81b7d05678f1bb6d16d7155e553933
Roberto Morais <fuenrob@gmail.com>
2024-11-25 19:18:24 +0100

    Bump version to 0.0.4 in configuration

--- config/config.go
+++ config/config.go
@@ -7,4 +7,4 @@ import (
 
 var VersionsDir = filepath.Join(os.Getenv("HOME"), ".govm", "versions") // Cambia a tu ruta de versiones
 const GoBinDir = "/usr/local/go/bin"                                    // Ruta donde deben copiarse los archivos binarios
-const Version string = "0.0.2"
+const Version string = "0.0.4"
